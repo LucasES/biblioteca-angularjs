192e5fc3b1e59cd14b4aef8efd566950701941ed
LucasES <lucas.compufc@gmail.com>
2015-08-10 14:55:48 -0300

    Projeto de uma biblioteca utilizando AngularJS

---
+++ Biblioteca/js/app.js
@@ -0,0 +1,45 @@
+﻿(function() {
+    var app = angular.module('MyApp', ['library-directives']);
+
+    app.controller("BookStore", function ($scope) {
+        $scope.filtro = "";
+        $scope.mostrar = false;        
+        $scope.livros = [
+            { ISBN: "5674789", Name: "Asp.Net MVC", Price: 560, Quantity: 20 },
+            { ISBN: "4352134", Name: "AngularJS", Price: 450, Quantity: 25 },
+            { ISBN: "2460932", Name: "Javascript", Price: 180, Quantity: 15 }
+        ];
+        $scope.editing = false;
+        $scope.addItem = function (item) {
+            $scope.livros.push(item);
+            $scope.item = {};
+        }
+
+        $scope.totalPrice = function () {
+            var total = 0;
+            for (count = 0; count < $scope.livros.length; count++) {
+                total += $scope.livros[count].Price * $scope.livros[count].Quantity;
+            }
+            return total;
+        }
+
+        $scope.removeItem = function (index) {
+            $scope.livros.splice(index, 1);
+        }
+        $scope.editItem = function (index) {
+            $scope.editing = $scope.livros.indexOf(index);
+
+        }
+        $scope.saveField = function (index) {
+            if ($scope.editing !== false) {
+                $scope.editing = false;
+            }
+        };
+
+        $scope.cancel = function (index) {
+            if ($scope.editing !== false) {
+                $scope.editing = false;
+            }
+        };
+    });
+})();
---
+++ Biblioteca/js/library-directives.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    var app = angular.module('library-directives', []);
+
+    app.directive('formularioLivro', function () {
+        return {
+            restrict: 'A',
+            templateUrl: 'templates/formulario-livro.html'
+        }
+    })
+    app.directive('dadosLivros', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/dados-livros.html'
+        }
+    })
+    app.directive('filtrarLivro', function () {
+        return {
+            restrict: 'AE',
+            templateUrl: 'templates/filtrar-livro.html'
+        }
+    });
+
+    app.directive('gerarJson', function () {
+        return {
+            restrict: 'AE',
+            templateUrl: 'templates/gerar-json.html'
+        }
+    });
+
+})();
